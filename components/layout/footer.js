2bf91586cfaee0ce739f77c481d182d8aae08fd2
Aviraj Sharma Ramhith <aramhith@proximitybbdo.fr>
2021-08-03 14:47:20 +0400

    footer

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -1,8 +1,32 @@
-import styled from "styled-components";
+import React, { useState, useEffect, Fragment } from "react";
 import Link from "next/link";
+import styled from "styled-components";
+import router from "next/router";
+import data from "../../public/data/data.json";
+import Image from "next/dist/client/image";
 
 const Footer = () => {
-  return <footer></footer>;
+  const [footerDetails, setFooterDetails] = useState([]);
+
+  useEffect(() => {
+    if (data.charging) {
+      setFooterDetails(data.footer);
+    }
+  }, []);
+  console.log(footerDetails);
+  return (
+    <FooterContainer>
+      {footerDetails.length > 0 && (
+        <Fragment>
+          <LogoContainer></LogoContainer>
+        </Fragment>
+      )}
+    </FooterContainer>
+  );
 };
+
+const FooterContainer = styled.footer``;
+
+const LogoContainer = styled.footer``;
 
 export default Footer;
